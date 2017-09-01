c4aed473597dfc4768853b0e5a5fae730f9091bb
JasonLev <jasond.levy@gmail.com>
2017-08-31 19:23:58 -0700

    first commit with boiler plate create-react-app ready to fetch data

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chewbaca from './Chewbaca';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      starWarsName: "Chewbaca"
+    }
+  }
+  componentDidMount(){
+    let url = "https://swapi.co/api/people/1";
+    fetch(url)
+      // .then((response) => {
+      //   response.json()
+      //   .then((data) => {
+      //     this.setState(
+      //       {starWarsName: data.name}
+      //     );
+      //   });
+        .then((response) => {
+          return response.json()
+        })
+        .then(data => {
+          console.log("this is: ", this);
+          // console.log("Star Wars Character: ", data.name);
+          this.setState({
+            starWarsName: data.name
+          });
+        });
+    // });
+  }
+  render() {
+    return (
+      <div className="App">
+        <body className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </body>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>My favorite Star Wars character is: {this.state.starWarsName}</h1>
+        <Chewbaca />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Chewbaca.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Chewbaca extends Component {
+  render(){
+    return <h1>Chewbaca says, 'RERERERERRE'</h1>;
+  }
+}
+export default Chewbaca;
